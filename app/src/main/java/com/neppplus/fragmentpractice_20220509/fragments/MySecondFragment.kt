d22922b79b19a6f8c2646639b41d0034b7336f99
nepp.plus <nepp.plus@gamil.com>
2022-05-09 10:52:45 +0900

    액티비티 xml 상에 두번째 프래그먼트 작성   - 프래그먼트에서 들고있는 화면 ( Context / Activity )을 던져줄 일이있다면, requireContext() or requireActivity()로 사용하자

--- app/src/main/java/com/neppplus/fragmentpractice_20220509/fragments/MySecondFragment.kt
+++ app/src/main/java/com/neppplus/fragmentpractice_20220509/fragments/MySecondFragment.kt
@@ -1,30 +1,30 @@
 package com.neppplus.fragmentpractice_20220509.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.neppplus.fragmentpractice_20220509.R
-import kotlinx.android.synthetic.main.fragment_my_first.*
+import kotlinx.android.synthetic.main.fragment_my_second.*
 
-class MyFirstFragment : Fragment() {
+class MySecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_first, container, false)
+        return inflater.inflate(R.layout.fragment_my_second, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 //        이 프래그먼트를 들고 있는 액티비티가 완전히 만들어진 시점 ( Activity 생명주기 활용 ) > 다른 함수로 바꿔서 설명 예정
 //        이벤트 처리, 데이터 보여주기 등등 "동작"
-        logBtn.setOnClickListener {
-            Log.d("프래그먼트", "첫번째 프래그먼트 로그 찍힘")
+        toastBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "두번째 프래그먼트입니다.", Toast.LENGTH_SHORT).show()
         }
     }
 }
