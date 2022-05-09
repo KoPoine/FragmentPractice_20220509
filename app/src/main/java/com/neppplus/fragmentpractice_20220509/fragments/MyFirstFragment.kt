b307c988a0e157e1e97a13402e7084387e3925e8
nepp.plus <nepp.plus@gamil.com>
2022-05-09 09:55:58 +0900

    프래그먼트 클래스에 onCreateView 함수 오버라이딩 => view를 프래그먼트에 붙여줬다.

--- app/src/main/java/com/neppplus/fragmentpractice_20220509/fragments/MyFirstFragment.kt
+++ app/src/main/java/com/neppplus/fragmentpractice_20220509/fragments/MyFirstFragment.kt
@@ -1,4 +1,19 @@
 package com.neppplus.fragmentpractice_20220509.fragments
 
-class MyFirstFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.fragmentpractice_20220509.R
+
+class MyFirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_first, container, false)
+    }
 }
