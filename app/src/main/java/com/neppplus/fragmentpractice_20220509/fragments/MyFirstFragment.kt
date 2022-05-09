45f19e06df8d4e0bdaae70a5f50af06b4d09f1ee
nepp.plus <nepp.plus@gamil.com>
2022-05-09 10:09:30 +0900

    프래그먼트 클래스에 onActivityCreated 함수 오버라이딩 => 프래그먼트 이벤트 처리

--- app/src/main/java/com/neppplus/fragmentpractice_20220509/fragments/MyFirstFragment.kt
+++ app/src/main/java/com/neppplus/fragmentpractice_20220509/fragments/MyFirstFragment.kt
@@ -16,4 +16,11 @@ class MyFirstFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_my_first, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        이 프래그먼트를 들고 있는 액티비티가 완전히 만들어진 시점 ( Activity 생명주기 활용 ) > 다른 함수로 바꿔서 설명 예정
+//        이벤트 처리, 데이터 보여주기 등등 "동작"
+
+    }
 }
